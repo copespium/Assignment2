c07a9d7ee078d071b88447b38ab566dc14bac7c3
Marven <marventwj@hotmail.com>
2023-02-25 17:18:46 +0800

    Revert "hoho"
    This reverts commit 2ad2c42886b376d316d66185ee44361bd289e1b4.

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         Timer t = new Timer();
         t.schedule(task, 3000);
         System.out.println("test2");
-        System.out.println("test123");
+        System.out.println("test");
     }
     private void finishscreen() {
         this.finish();
