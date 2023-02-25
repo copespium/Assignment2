7384a0b1f2937f3046004a4e841fb917b64f93c0
minu <reginanhm@gmail.com>
2023-02-25 21:49:07 +0800

    Images

--- app/src/main/java/com/example/assignment2/numeracy.java
+++ app/src/main/java/com/example/assignment2/numeracy.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class Home extends AppCompatActivity {
-
-    private FirebaseAuth mAuth;
+public class numeracy extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_numeracy);
     }
 }
