5e1870d8d5ef5d7b3f2f7e920f5547202ffe6938
minu <reginanhm@gmail.com>
2023-02-26 22:18:49 +0800

    Profile, sign out

--- app/src/main/java/com/example/assignment2/Menu.java
+++ app/src/main/java/com/example/assignment2/Menu.java
@@ -3,11 +3,16 @@ package com.example.assignment2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,12 +22,17 @@ import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.w3c.dom.Text;
+
 public class Menu extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
     private ImageButton ib;
 
     private Button myButton;
+
+    private TextView myTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,12 +47,45 @@ public class Menu extends AppCompatActivity implements View.OnClickListener {
         myButton = (Button) findViewById(R.id.profilebutton);
         myButton.setOnClickListener(this);
 
+        myTextView = (TextView) findViewById(R.id.signOutButton);
+
+        myTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(Menu.this);
+                builder.setTitle("Username, you have overall 120 points");
+
+                // Set up the input
+                //final EditText input = new EditText(Menu.this);
+                // Specify the type of input expected
+                //input.setInputType(InputType.TYPE_CLASS_NUMBER);
+                //builder.setView(input);
+
+                // Set up the buttons
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent3 = new Intent(Menu.this, Home.class);
+                        startActivity(intent3);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+
+            }
+        });
 
     }
 
     @Override
-    public void onClick(View v){
-        switch (v.getId()){
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.numeracyButton:
                 //Toast.makeText(Menu.this, "numeracyButton button pressed", Toast.LENGTH_LONG).show();
                 //System.out.println("numeracyButton button pressed");
@@ -60,12 +103,17 @@ public class Menu extends AppCompatActivity implements View.OnClickListener {
                 Intent intent2 = new Intent(Menu.this, Profile.class);
                 startActivity(intent2);
                 break;
+            case R.id.signOutButton:
+                //Toast.makeText(Menu.this, "historyButton button pressed", Toast.LENGTH_LONG).show();
+                //Intent intent3 = new Intent(Menu.this, Home.class);
+                //startActivity(intent3);
+                break;
         }
     }
 
 
-    //TODO
-    private void changePassword(){
+        //TODO
+        private void changePassword () {
         mAuth = FirebaseAuth.getInstance();
 
         System.out.println("logged in as: " + mAuth.getCurrentUser().getUid() + ", " + mAuth.getCurrentUser().getEmail());
@@ -99,4 +147,4 @@ public class Menu extends AppCompatActivity implements View.OnClickListener {
                     }
                 });
     }
-}
+    }
